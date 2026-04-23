9fbfd914cede680fbed6a8930d8a49ad379e999a
Jeremy Leland <jeremy.leland@gmail.com>
2026-04-23 11:23:43 -0700

    Monster moves toward player

--- test/playerVsMonster.js
+++ test/playerVsMonster.js
@@ -6,11 +6,7 @@ import { vec2 } from '../lib/gl-matrix.js';
 
 import * as Entities from '../src/Entities.js';
 
-const image = new Image();
-image.src = './images/green1.png';
-await image.decode();
-
-const ratio = image.width / image.height;
+const EnemySpeed = 0.002;
 
 const player = {
   type: 'player',
@@ -33,9 +29,42 @@ gameCanvas.bounds = [ -3, -3, 3, 3 ];
 gameCanvas.backgroundColor = '#123';
 
 gameCanvas.update = ( dt ) => {
-  if ( player.animation ) {
-    player.animation.time += dt;
-  }
+  entities.forEach( entity => {
+    if ( entity.animation ) {
+      entity.animation.time += dt;
+    }
+
+    if ( entity.type == 'monster' ) {
+      const moveVector = vec2.subtract( [], player.pos, entity.pos );
+      const distanceFrom = vec2.len( moveVector );
+      vec2.normalize( moveVector, moveVector );
+
+      entities.forEach( other => {
+        if ( entity != other ) {
+          // Opposite direction so we avoid other
+          const toOther = vec2.subtract( [], entity.pos, other.pos );
+          const distToOther = Math.max( 1e-6, vec2.len( toOther ) - entity.radius - other.radius );
+          vec2.normalize( toOther, toOther );
+
+          // This function is infinite at f(0) and close to 0 at f(1)
+          const weight = 1 / Math.tanh( 3 * distToOther ) - 1;
+
+          // TODO: Incorporate radius somehow, so bigger enemies push smaller ones around more?
+
+          // vec2.scaleAndAdd( avoid, avoid, toOther, weight );
+          vec2.scaleAndAdd( moveVector, moveVector, toOther, weight );
+        }
+      } );
+
+      const moveDist = sigma( distanceFrom ) * EnemySpeed * dt;
+      vec2.scaleAndAdd( entity.pos, entity.pos, moveVector, moveDist );
+    }
+  } );
+}
+
+// Play with different functions here?
+function sigma( x ) {
+  return Math.tanh( 10 * x );
 }
 
 gameCanvas.draw = ( ctx ) => {
@@ -51,7 +80,3 @@ document.addEventListener( 'keydown', e => {
     gameCanvas.toggle();
   }
 } );
-
-// gameCanvas.pointerMove = ( m ) => {
-//   gameCanvas.redraw();
-// }

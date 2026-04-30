038c3dbf323ff2f0c0c6a0716cc058e0964e0b72
Jeremy Leland <jeremy.leland@gmail.com>
2026-04-30 14:46:56 -0700

    Put monster movement code in World

--- src/World.js
+++ src/World.js
@@ -4,46 +4,121 @@ import { vec2 } from '../lib/gl-matrix.js';
 
 const PlayerSpeed = 0.003;
 
+const EnemyBiteDist = 0.4;
+const EnemyBiteDelay = 800;
+const EnemySpeed = 0.002;
 
 export class World {
-  player;
-  enemies = [];
-  bullets = [];
+  // player;
+  // enemies = [];
+  // bullets = [];
 
-  spawnPlayer( pos, vel ) {
-    return {
+  entities = [];
+
+  newPlayer( vals ) {
+    return Object.assign( {
       type: 'player',
-      pos: pos ?? [ 0, 0 ],
-      vel: vel ?? [ 0, 0 ],
+      pos: [ 0, 0 ],
       radius: 0.5,
       facing: 0,
       life: 100,
       weapons: [
         { type: 'pistol', angle: 0, delay: 0 },
       ],
-
-      input: { left: false, up: false, right: false, down: false },
-    };
+    }, vals );
   }
 
-  update( dt ) {
-    if ( this.player ) {
+  newMonster( vals ) {
+    return Object.assign( {
+      type: 'monster',
+      pos: [ 0, 0 ],
+      radius: 0.5,
+      facing: 0,
+      delay: 0,
+      life: 1,
+    }, vals );
+  }
+
+  update( dt, input ) {
+
+    const player = this.entities.find( e => e.type === 'player' );
+
+    if ( player ) {
       const moveVector = [
-        ( this.player.input.left ? -1 : 0 ) + ( this.player.input.right ? 1 : 0 ),
-        ( this.player.input.up   ? -1 : 0 ) + ( this.player.input.down  ? 1 : 0 ),
+        ( input.left ? -1 : 0 ) + ( input.right ? 1 : 0 ),
+        ( input.up   ? -1 : 0 ) + ( input.down  ? 1 : 0 ),
       ];
       vec2.normalize( moveVector, moveVector );
 
-      vec2.scaleAndAdd( this.player.pos, this.player.pos, moveVector, PlayerSpeed * dt );
+      vec2.scaleAndAdd( player.pos, player.pos, moveVector, PlayerSpeed * dt );
     }
+
+    this.entities.forEach( entity => {
+      if ( entity.animation ) {
+        entity.animation.time += dt;
+      }
+
+      if ( entity.delay > 0 ) {
+        entity.delay -= dt;
+      }
+      else {
+        if ( entity.type === 'monster' ) {
+          // Move
+          const moveVector = vec2.subtract( [], player.pos, entity.pos );
+          const distanceFrom = vec2.len( moveVector );
+
+          vec2.normalize( moveVector, moveVector );   // this is defaulting a weight of 1, maybe change later
+
+          entity.facing = moveVector[ 0 ] <= 0 ? Entities.Facing.Left : Entities.Facing.Right;
+
+          this.entities.forEach( other => {
+            if ( entity != other && ( other.type === 'player' || other.type === 'monster' ) ) {
+              // Opposite direction so we avoid other
+              const toOther = vec2.subtract( [], entity.pos, other.pos );
+              const distToOther = Math.max( 1e-6, vec2.len( toOther ) - entity.radius - other.radius );
+              vec2.normalize( toOther, toOther );
+
+              // This function is infinite at f(0) and close to 0 at f(1)
+              const weight = 1 / Math.tanh( 3 * distToOther ) - 1;
+
+              // TODO: Incorporate radius somehow, so bigger enemies push smaller ones around more?
+
+              // vec2.scaleAndAdd( avoid, avoid, toOther, weight );
+              vec2.scaleAndAdd( moveVector, moveVector, toOther, weight );
+            }
+          } );
+
+          // Cap at length of 1 (normalizing might make short vectors longer) (should it use other weighting?)
+          const moveLength = vec2.len( moveVector );
+          if ( moveLength > 1 ) {
+            moveVector[ 0 ] /= moveLength;
+            moveVector[ 1 ] /= moveLength;
+          }
+
+          const moveDist = Math.tanh( 10 * distanceFrom ) * EnemySpeed * dt;
+          vec2.scaleAndAdd( entity.pos, entity.pos, moveVector, moveDist );
+
+          // Attack (if in range)
+          if ( distanceFrom < player.radius + entity.radius + EnemyBiteDist ) {
+            entity.animation = { name: 'bite', time: 0 };
+            entity.delay += EnemyBiteDelay;
+
+            console.log( 'Bite!' );
+            // TODO: Do actual bite (damage player, etc)
+          }
+          else {
+            if ( entity.animation?.name != 'walk' ) {
+              console.log( 'Walk!' );
+              entity.animation = { name: 'walk', time: 0 };
+            }
+          }
+        }
+      }
+    } );
   }
 
   draw( ctx ) {
-    if ( this.player) {
-      Entities.draw( ctx, this.player );
-    }
-
-    this.enemies.forEach( enemy => Entities.draw( ctx, enemy ) );
-    this.bullets.forEach( bullet => Entities.draw( ctx, bullet ) );
+    // TODO: Should the entities draw code just go here, then?
+    this.entities.forEach( entity => Entities.draw( ctx, entity ) );
   }
 }
--- test/testWorld.js
+++ test/testWorld.js
@@ -12,6 +12,7 @@ const PlayerSpeed = 0.003;
 const PlayerHandSpeed = 0.003;
 const PlayerTargetDeltaAngle = 0.1;
 
+
 const PistolDelay = 500;
 const PistolRange = 2;
 const PistolBulletSpeed = 0.01;
@@ -19,13 +20,13 @@ const PistolBulletDamage = 1;
 
 const world = new World();
 
-world.player = world.spawnPlayer();
+world.entities.push( world.newPlayer() );
 
-world.enemies.push(
-  { type: 'monster', pos: [ -1, 1 ], radius: 0.3, facing: 1, life: 1 },
-  { type: 'monster', pos: [ -2, 2 ], radius: 0.4, facing: 0, life: 2 },
-  { type: 'monster', pos: [  2, 1 ], radius: 0.5, facing: 1, life: 3 },
-  { type: 'monster', pos: [  1, 2 ], radius: 0.6, facing: 0, life: 4 },
+world.entities.push(
+  world.newMonster( { pos: [ -1, 1 ], radius: 0.3, facing: 1, life: 1 } ),
+  world.newMonster( { pos: [ -2, 2 ], radius: 0.4, facing: 0, life: 2 } ),
+  world.newMonster( { pos: [  2, 1 ], radius: 0.5, facing: 1, life: 3 } ),
+  world.newMonster( { pos: [  1, 2 ], radius: 0.6, facing: 0, life: 4 } ),
 );
 
 
@@ -33,8 +34,15 @@ const gameCanvas = new GameCanvas();
 gameCanvas.bounds = [ -3, -3, 3, 3 ];
 gameCanvas.backgroundColor = '#123';
 
+const input = {
+  left:   false,
+  up:     false,
+  right:  false,
+  down:   false,
+};
+
 gameCanvas.update = ( dt ) => {
-  world.update( dt );
+  world.update( dt, input );
 }
 
 gameCanvas.draw = ( ctx ) => {
@@ -47,17 +55,17 @@ gameCanvas.start();
 
 const keyDownAction = {
   ' ': () => gameCanvas.toggle(),
-  'w': () => world.player.input.up = true,
-  'a': () => world.player.input.left = true,
-  's': () => world.player.input.down = true,
-  'd': () => world.player.input.right = true,
+  'w': () => input.up = true,
+  'a': () => input.left = true,
+  's': () => input.down = true,
+  'd': () => input.right = true,
 }
 
 const keyUpAction = {
-  'w': () => world.player.input.up = false,
-  'a': () => world.player.input.left = false,
-  's': () => world.player.input.down = false,
-  'd': () => world.player.input.right = false,
+  'w': () => input.up = false,
+  'a': () => input.left = false,
+  's': () => input.down = false,
+  'd': () => input.right = false,
 }
 
 document.addEventListener( 'keydown', e => keyDownAction[ e.key ]?.() );

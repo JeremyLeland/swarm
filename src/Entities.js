ccd0cfcebcb5c23fee3fc236a8de50461bdde3f2
Jeremy Leland <jeremy.leland@gmail.com>
2026-04-30 21:04:47 -0700

    Don't apply facing an animations to weapons

--- src/Entities.js
+++ src/Entities.js
@@ -45,33 +45,38 @@ export function draw( ctx, entity ) {
     const ratio = image.width / image.height;
     ctx.scale( ratio * entity.radius * 2, entity.radius * 2 );
 
-    // player image faces left, need to flip if facing right
-    // TODO: Would it make more sense to have everything face right (angle = 0) by default?
-    if ( entity.facing ) {
-      const dir = entity.facing == Facing.Left ? 1 : -1;
-      ctx.scale( dir, 1 );
+
+    // Facing and animations should only apply to main entity, not weapons
+    ctx.save(); {
+      // player image faces left, need to flip if facing right
+      // TODO: Would it make more sense to have everything face right (angle = 0) by default?
+      if ( entity.facing ) {
+        const dir = entity.facing == Facing.Left ? 1 : -1;
+        ctx.scale( dir, 1 );
+      }
+
+      //
+      // Animations
+      //
+      if ( entity.animation?.name == 'walk' ) {
+        const walkOffset = 0.1 * Math.sin( entity.animation.time / 100 );
+        ctx.translate( 0, -walkOffset / 2 );
+        ctx.scale( 1, 1 + walkOffset );
+      }
+
+      if ( entity.animation?.name == 'bite' ) {
+        const biteTime = Math.min( Math.PI, entity.animation.time / 100 );
+        const biteOffset = 0.5 * Math.sin( biteTime );
+
+        ctx.translate( -biteOffset / 2, 0 );
+
+        // scaleX, skewY, skewX, scaleY, translateX, translateY
+        ctx.transform( 1, 0, biteOffset, 1, 0, 0 );
+      }
+
+      ctx.drawImage( image, -0.5, -0.5, 1, 1 );
     }
-
-    //
-    // Animations
-    //
-    if ( entity.animation?.name == 'walk' ) {
-      const walkOffset = 0.1 * Math.sin( entity.animation.time / 100 );
-      ctx.translate( 0, -walkOffset / 2 );
-      ctx.scale( 1, 1 + walkOffset );
-    }
-
-    if ( entity.animation?.name == 'bite' ) {
-      const biteTime = Math.min( Math.PI, entity.animation.time / 100 );
-      const biteOffset = 0.5 * Math.sin( biteTime );
-
-      ctx.translate( -biteOffset / 2, 0 );
-
-      // scaleX, skewY, skewX, scaleY, translateX, translateY
-      ctx.transform( 1, 0, biteOffset, 1, 0, 0 );
-    }
-
-    ctx.drawImage( image, -0.5, -0.5, 1, 1 );
+    ctx.restore();
 
     entity.weapons?.forEach( weapon => {
       ctx.rotate( weapon.angle );
